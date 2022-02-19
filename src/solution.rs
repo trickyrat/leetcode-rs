0ab6f4e139cf091a768f4e7c279a8d9ebcaad204
trickyrat <wangjian0513@outlook.com>
2022-02-19 23:40:43 +0800

    添加2. Add Two Numbers

--- src/solution.rs
+++ src/solution.rs
@@ -1,4 +1,20 @@
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode { next: None, val }
+  }
+}
+
 impl Solution {
+  /**
+   * 1. Two Sum
+   */
   pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut hashmap: std::collections::HashMap<i32, usize> =
       std::collections::HashMap::with_capacity(nums.len());
@@ -11,5 +27,37 @@ impl Solution {
       hashmap.insert(nums[i], i);
     }
     panic!("???")
+  }
+
+  pub fn add_two_numbers(
+    l1: Option<Box<ListNode>>,
+    l2: Option<Box<ListNode>>,
+  ) -> Option<Box<ListNode>> {
+    carried(l1, l2, 0)
+  }
+
+  fn carried(
+    l1: Option<Box<ListNode>>,
+    l2: Option<Box<ListNode>>,
+    mut carry: i32,
+  ) -> Option<Box<ListNode>> {
+    if l1.is_none() && l2.is_none() && carry == 0 {
+      None
+    } else {
+      Some(Box::new(ListNode {
+        next: carried(
+          l1.and_then(|x| {
+            carry += x.val;
+            x.next
+          }),
+          l2.and_then(|x| {
+            carry += x.val;
+            x.next
+          }),
+          carry / 10,
+        ),
+        val: carry % 10,
+      }))
+    }
   }
 }
