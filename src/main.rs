07f8a1c1b1ed44e5de8f117c0ee1896044365f5b
trickyrat <wangjian0513@outlook.com>
2022-03-02 01:13:19 +0800

    添加测试样例

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+pub mod solution;
+use solution::*;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn test_two_sum() {
+    assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+    assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+    assert_eq!(two_sum(vec![3, 3], 6), vec![0, 1]);
+}
+
+#[test]
+fn test_reverse_int() {
+    assert_eq!(reverse_int(123), 321);
+    assert_eq!(reverse_int(-123), -321);
+    assert_eq!(reverse_int(120), 21);
+    assert_eq!(reverse_int(100), 1);
+    assert_eq!(reverse_int(2147483647), 0);
+    assert_eq!(reverse_int(-2147483648), 0);
+}
+
+#[test]
+fn test_complex_number_multiply() {
+    assert_eq!(
+        complex_number_multiply(String::from("1+1i"), String::from("1+1i")),
+        String::from("0+2i")
+    );
+    assert_eq!(
+        complex_number_multiply(String::from("1+-1i"), String::from("1+-1i")),
+        String::from("0+-2i")
+    );
 }
--- src/solution.rs
+++ src/solution.rs
@@ -3,90 +3,91 @@ pub struct ListNode {
   pub val: i32,
   pub next: Option<Box<ListNode>>,
 }
-
 impl ListNode {
   #[inline]
-  fn new(val: i32) -> Self {
+  pub fn new(val: i32) -> Self {
     ListNode { next: None, val }
   }
 }
 
-impl Solution {
-  /**
-   * 1. Two Sum
-   */
-  pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut hashmap: std::collections::HashMap<i32, usize> =
-      std::collections::HashMap::with_capacity(nums.len());
-    for i in 0..nums.len() {
-      if let Some(k) = hashmap.get(&(target - nums[i])) {
-        if *k != i {
-          return vec![*k as i32, i as i32];
-        }
+/**
+ * 1. Two Sum
+ */
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+  let mut hashmap: std::collections::HashMap<i32, usize> =
+    std::collections::HashMap::with_capacity(nums.len());
+  for i in 0..nums.len() {
+    if let Some(k) = hashmap.get(&(target - nums[i])) {
+      if *k != i {
+        return vec![*k as i32, i as i32];
       }
-      hashmap.insert(nums[i], i);
     }
-    panic!("???")
+    hashmap.insert(nums[i], i);
   }
+  panic!("???")
+}
 
-  pub fn add_two_numbers(
-    l1: Option<Box<ListNode>>,
-    l2: Option<Box<ListNode>>,
-  ) -> Option<Box<ListNode>> {
-    carried(l1, l2, 0)
+/**
+ * 2. 两数相加
+ */
+pub fn add_two_numbers(
+  l1: Option<Box<ListNode>>,
+  l2: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+  carried(l1, l2, 0)
+}
+fn carried(
+  l1: Option<Box<ListNode>>,
+  l2: Option<Box<ListNode>>,
+  mut carry: i32,
+) -> Option<Box<ListNode>> {
+  if l1.is_none() && l2.is_none() && carry == 0 {
+    None
+  } else {
+    Some(Box::new(ListNode {
+      next: carried(
+        l1.and_then(|x| {
+          carry += x.val;
+          x.next
+        }),
+        l2.and_then(|x| {
+          carry += x.val;
+          x.next
+        }),
+        carry / 10,
+      ),
+      val: carry % 10,
+    }))
   }
-
-  fn carried(
-    l1: Option<Box<ListNode>>,
-    l2: Option<Box<ListNode>>,
-    mut carry: i32,
-  ) -> Option<Box<ListNode>> {
-    if l1.is_none() && l2.is_none() && carry == 0 {
-      None
-    } else {
-      Some(Box::new(ListNode {
-        next: carried(
-          l1.and_then(|x| {
-            carry += x.val;
-            x.next
-          }),
-          l2.and_then(|x| {
-            carry += x.val;
-            x.next
-          }),
-          carry / 10,
-        ),
-        val: carry % 10,
-      }))
+}
+/**
+ * 7. 整数转换
+ */
+pub fn reverse_int(x: i32) -> i32 {
+  let mut x = x;
+  let mut res = 0;
+  while x != 0 {
+    if res < i32::MIN / 10 || res > i32::MAX / 10 {
+      return 0;
     }
+    let digit = x % 10;
+    x /= 10;
+    res = res * 10 + digit;
   }
+  res
+}
 
-  /**
-   * 7. 整数转换
-   */
-  fn reverse_int(x: i32) -> i32 {
-    let mut x = x;
-    let mut res = 0;
-    while x != 0 {
-      if res < i32::MIN / 10 || res > i32::MAX / 10 {
-        return 0;
-      }
-      let mut digit = x % 10;
-      x /= 10;
-      res = res * 10 + digit;
-    }
-    res
-  }
-
-  fn complex_number_multiply(num1: String, num2: String) -> String {
-    let &complex1 = &num1[..num1.len() - 1].split_once('+').unwrap();
-    let &complex2 = &num2[..num2.len() - 1].split_once('+').unwrap();
-    let (real1, imag1): (i32, i32) = (complex1.0.parse().unwrap(), complex1.1.parse().unwrap());
-    let (real2, imag2): (i32, i32) = (complex2.0.parse().unwrap(), complex2.1.parse().unwrap());
-    format!(
-      "{}+{}i",
-      real1 * real2 - imag1 * imag2,
-      real1 * imag2 + imag1 * real2
-    )
-  }
+/**
+ * 537. 复数乘法
+ */
+pub fn complex_number_multiply(num1: String, num2: String) -> String {
+  let &complex1 = &num1[..num1.len() - 1].split_once('+').unwrap();
+  let &complex2 = &num2[..num2.len() - 1].split_once('+').unwrap();
+  let (real1, imag1): (i32, i32) = (complex1.0.parse().unwrap(), complex1.1.parse().unwrap());
+  let (real2, imag2): (i32, i32) = (complex2.0.parse().unwrap(), complex2.1.parse().unwrap());
+  format!(
+    "{}+{}i",
+    real1 * real2 - imag1 * imag2,
+    real1 * imag2 + imag1 * real2
+  )
 }
