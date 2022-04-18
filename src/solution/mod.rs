2925be55b7bfec51948f423bd1456875b51c300b
trickyrat <wangjian0513@outlook.com>
2022-04-19 00:50:26 +0800

    添加386. 字典序排数

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -194,6 +194,30 @@ fn test_count_numbers_with_unique_digits() {
   assert_eq!(count_numbers_with_unique_digits(0), 1);
 }
 
+// 386. 字典序排数
+pub fn lexical_order(n: i32) -> Vec<i32> {
+  let mut ret: Vec<i32> = Vec::with_capacity(n as usize);
+  let mut num = 1;
+  for _ in 0..n {
+    ret.push(num);
+    if num * 10 <= n {
+      num *= 10;
+    } else {
+      while num % 10 == 9 || num + 1 > n {
+        num /= 10;
+      }
+      num += 1;
+    }
+  }
+  ret
+}
+
+#[test]
+fn test_lexical_order() {
+  assert_eq!(lexical_order(13), vec![1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]);
+  assert_eq!(lexical_order(2), vec![1, 2]);
+}
+
 /**
  * 498.对角线遍历
  */
