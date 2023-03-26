f665b4b96ef5f76030e6f1133a9bf31c67496d88
trickyrat <wangjian0513@outlook.com>
2023-03-26 21:35:28 +0800

    fix overflow

--- src/leetcode/solution.rs
+++ src/leetcode/solution.rs
@@ -198,18 +198,18 @@ pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
 
 /// 35. Search Insert Position
 pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    let n = nums.len();
+    let n = nums.len() as i32;
     let (mut left, mut right, mut res) = (0, n - 1, n);
     while left <= right {
         let mid = left + (right - left) / 2;
-        if nums[mid] >= target {
+        if nums[mid as usize] >= target {
             res = mid;
             right = mid - 1;
         } else {
             left = mid + 1;
         }
     }
-    res as i32
+    res
 }
 
 /// 86.Partition List
@@ -1854,6 +1854,7 @@ mod tests {
 
     #[test]
     fn test_search_insert() {
+        assert_eq!(search_insert(vec![1, 3, 5, 6], 0), 0);
         assert_eq!(search_insert(vec![1, 3, 5, 6], 5), 2);
         assert_eq!(search_insert(vec![1, 3, 5, 6], 2), 1);
         assert_eq!(search_insert(vec![1, 3, 5, 6], 7), 4);
