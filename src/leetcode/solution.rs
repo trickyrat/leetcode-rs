5d9e71581ab0f1cf00dc171c3be6358408ceb406
trickyrat <wangjian0513@outlook.com>
2023-10-18 21:34:22 +0800

    update add_two_numbers

--- src/leetcode/solution.rs
+++ src/leetcode/solution.rs
@@ -28,31 +28,26 @@ pub fn add_two_numbers(
     l1: Option<Box<ListNode>>,
     l2: Option<Box<ListNode>>,
 ) -> Option<Box<ListNode>> {
-    fn carried(
-        l1: Option<Box<ListNode>>,
-        l2: Option<Box<ListNode>>,
-        mut carry: i32,
-    ) -> Option<Box<ListNode>> {
-        if l1.is_none() && l2.is_none() && carry == 0 {
-            None
-        } else {
-            Some(Box::new(ListNode {
-                next: carried(
-                    l1.and_then(|x| {
-                        carry += x.val;
-                        x.next
-                    }),
-                    l2.and_then(|x| {
-                        carry += x.val;
-                        x.next
-                    }),
-                    carry / 10,
-                ),
-                val: carry % 10,
-            }))
+    let (mut l1, mut l2) = (l1, l2);
+    let mut dummy = ListNode::new(0);
+    let mut curr = &mut dummy;
+    let mut carry = 0;
+
+    while l1.is_some() || l2.is_some() || carry != 0 {
+        let mut sum = carry;
+        if let Some(node) = l1 {
+            sum += node.val;
+            l1 = node.next;
         }
+        if let Some(node) = l2 {
+            sum += node.val;
+            l2 = node.next;
+        }
+        carry = sum / 10;
+        curr.next = Some(Box::new(ListNode::new(sum % 10)));
+        curr = curr.next.as_mut().unwrap();
     }
-    carried(l1, l2, 0)
+    dummy.next
 }
 
 /// 3. Longest Substring Without Repeating Characters
