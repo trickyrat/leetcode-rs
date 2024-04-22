a2ab06cf628c734a1c72c683107e19987923ecc2
trickyrat <wangjian0513@outlook.com>
2024-04-22 21:14:17 +0800

    feat: Add 377. Combination Sum IV

--- src/leetcode/solution.rs
+++ src/leetcode/solution.rs
@@ -417,6 +417,20 @@ pub fn count_numbers_with_unique_digits(n: i32) -> i32 {
     res
 }
 
+/// 377. Combination Sum IV
+pub fn combination_sum4(nums: Vec<i32>, target: i32) -> i32 {
+    let mut dp = vec![0; (target + 1) as usize];
+    dp[0] = 1;
+    for i in 1..=target {
+        for num in nums.iter() {
+            if *num <= i {
+                dp[i as usize] += dp[(i - num) as usize];
+            }
+        }
+    }
+    dp[target as usize]
+}
+
 /// 386.Lexicographical Numbers
 pub fn lexical_order(n: i32) -> Vec<i32> {
     let mut ret: Vec<i32> = Vec::with_capacity(n as usize);
@@ -2246,6 +2260,12 @@ mod tests {
     fn test_count_numbers_with_unique_digits() {
         assert_eq!(count_numbers_with_unique_digits(2), 91);
         assert_eq!(count_numbers_with_unique_digits(0), 1);
+    }
+
+    #[test]
+    fn test_combination_sum4() {
+        assert_eq!(combination_sum4(vec![1, 2, 3], 4), 7);
+        assert_eq!(combination_sum4(vec![], 3), 0);
     }
 
     #[test]
